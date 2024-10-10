4c1c828150f3ccbaf4093fbfe7b1ba1f2b8f698a
Gonzalo <gonzaloperezreich@gmail.com>
2024-10-09 22:54:11 -0300

    cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 const allowedOrigins = [
   'http://localhost:3000',
   'https://educ-app-cl-ient-1syj0gvjz-gonzaloperezreichs-projects.vercel.app',
-  'https://educ-app-cl-ient-bfylkxt0t-gonzaloperezreichs-projects.vercel.app'
+  'https://educ-app-cl-ient-bfylkxt0t-gonzaloperezreichs-projects.vercel.app',
+  'https://educ-app-htkh.vercel.app',
 ];
 
 app.use(cors({
